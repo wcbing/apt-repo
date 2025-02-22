ea65110241d1ab8a549ef1dc8268306de11eb7ce
wcbing <cbing2002@outlook.com>
2025-02-22 21:12:33 +0800

    refactor: github-releases, arch name, multithreading
    rewrite github downloader and configuration to store version tag in sqlite.
    add 'all' arch, change 'x86_64' to 'amd64'.
    add multithreading for downloader.

--- check_downloader.py
+++ check_downloader.py
@@ -2,7 +2,11 @@
 import subprocess
 import os
 import sqlite3
+import sys
 import logging
+import threading
+
+version_lock = threading.Lock()
 
 base_dir = "deb"
 logging.basicConfig(
@@ -33,48 +37,55 @@ def download(url):
 def check_download(name, version, url, arch):
     logging.info("%s:%s = %s", name, arch, version)
 
-    # connect to db
-    with sqlite3.connect(os.path.join("data", f"{base_dir}.db")) as conn:
-        cur = conn.cursor()
-        res = cur.execute(
-            f"SELECT version, url FROM {arch} WHERE name = ?", (name,)
-        ).fetchall()
-        if len(res):
-            local_version = res[0][0]
-            local_url = res[0][1]
-            if local_version != version:
-                print(f"Update: {name}:{arch} ({local_version} -> {version})")
-                download(url)
-                # wirte to db
-                cur.execute(
-                    f"UPDATE {arch} SET version = ?, url = ? WHERE name = ?",
-                    (version, url, name),
-                )
-                # remove old version
-                if local_url != url:  # 针对固定下载链接
-                    old_file_path = os.path.join(base_dir, local_url.split("?")[0])
-                    if os.path.exists(old_file_path):
-                        os.remove(old_file_path)
-        else:
-            print(f"AddNew: {name}:{arch} ({version})")
-            download(url)
-            # wirte to db
-            cur.execute(
-                f"INSERT INTO {arch}(name, version, url) VALUES (?, ?, ?)",
-                (name, version, url),
+    db_path = os.path.join("data", f"{base_dir}.db")
+    # get local version
+    with version_lock:
+        with sqlite3.connect(db_path) as conn:
+            res = (
+                conn.cursor()
+                .execute(f"SELECT version, url FROM '{arch}' WHERE name = ?", (name,))
+                .fetchone()
             )
-        conn.commit()
+    if res:
+        local_version, local_url = res
+        if local_version != version:
+            print(f"Update: {name}:{arch} ({local_version} -> {version})")
+            download(url)
+            # update database
+            with version_lock:
+                with sqlite3.connect(db_path) as conn:
+                    conn.cursor().execute(
+                        f"UPDATE '{arch}' SET version = ?, url = ? WHERE name = ?",
+                        (version, url, name),
+                    )
+                    # remove old version
+                    if local_url != url:  # 防止固定下载链接
+                        old_file_path = os.path.join(base_dir, local_url.split("?")[0])
+                        if os.path.exists(old_file_path):
+                            os.remove(old_file_path)
+                    conn.commit()
+    else:
+        print(f"AddNew: {name}:{arch} ({version})")
+        download(url)
+        # update database
+        with version_lock:
+            with sqlite3.connect(db_path) as conn:
+                conn.cursor().execute(
+                    f"INSERT INTO '{arch}'(name, version, url) VALUES (?, ?, ?)",
+                    (name, version, url),
+                )
+                conn.commit()
 
 
 if __name__ == "__main__":
-    args = os.sys.argv
+    args = sys.argv
     if len(args) == 5:
         check_download(args[1], args[2], args[3], args[4])
     elif len(args) == 4:
-        check_download(args[1], args[2], args[3], "x86_64")
+        check_download(args[1], args[2], args[3], "amd64")
     elif len(args) > 1:
         logging.error(f"Unknown Args: {args[1:]}")
     else:
         print(f"Usage: {args[0]} <package_name> <version> <url> [arch]")
         print("options:")
-        print("    arch: x86_64, arm64. default is x86_64")
+        print("    arch: amd64, arm64, all. default is amd64")
